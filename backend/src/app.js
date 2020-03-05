ad1086256d5d83b12b27e111dd47e66554bcb6dd
Jason Dip <ContactJasonDip@gmail.com>
2020-03-05 08:05:59 -0800

    Updated console text.

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,5 +4,5 @@ const server = require("./server/express");
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port}.`);
+    console.log(`Server is listening on port ${port}.`);
 });
