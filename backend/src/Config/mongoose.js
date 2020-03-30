5bdf31c068c3cd160c4d6b5c2e3b4caf88d3faa0
Jason Dip <ContactJasonDip@gmail.com>
2020-03-30 04:22:50 -0700

    changed server to only listen after db connection

--- backend/src/Config/mongoose.js
+++ backend/src/Config/mongoose.js
@@ -1,15 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose
-    .connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to MongoDB database.");
-    })
-    .catch(e => {
-        console.log("[ERROR] Unable to connect to the MongoDB database.");
-    });
+module.exports = mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,23 @@
 /* Load environment variables */
 require("./Config/env");
 
-/*  Connect to database  */
-require("./Config/mongoose");
-
 /*  Configure Express server  */
 const server = require("./Config/express");
 
-/*  begin listening on server  */
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`Server is listening on port ${port}.`);
-});
+/*  Connect to database  */
+require("./Config/mongoose")
+    .then(() => {
+        console.log("Connected to MongoDB database.");
+    })
+    .catch(e => {
+        console.log(
+            "[ERROR] Unable to connect to the MongoDB database: " + e.message
+        );
+    })
+    .then(() => {
+        /*  begin listening on server  */
+        const port = process.env.PORT || 3000;
+        server.listen(port, () => {
+            console.log(`Server is listening on port ${port}.`);
+        });
+    });
