19e50e366a82114a9003edc96b1b10e2adfdd24a
Jason Dip <ContactJasonDip@gmail.com>
2020-06-03 22:33:33 -0700

    add CLIENT_DOMAIN to backend env

--- backend/src/middleware/CORS.js
+++ backend/src/middleware/CORS.js
@@ -3,7 +3,7 @@ module.exports = (req, res, next) => {
     // need to set allow-origin with specific domain and allow-credentials
     // or else cross domain cookies will not work
     //res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_DOMAIN);
     res.setHeader("Access-Control-Allow-Credentials", "true");
 
     res.setHeader(
