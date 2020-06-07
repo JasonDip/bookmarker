c92a42b735f0b811701b92f91920a9c8b40dd4b4
Jason Dip <ContactJasonDip@gmail.com>
2020-06-06 23:34:31 -0700

    add createBookmark api to frontend

---
+++ frontend/src/redux/actions/bookmark.js
@@ -0,0 +1,27 @@
+import * as bookmarkApi from "../api/bookmark";
+import * as selectedCollectionDuck from "../ducks/selectedCollection";
+
+/* actions */
+// create a bookmark
+export const CREATE_BOOKMARK_PENDING =
+    "actions/bookmark/CREATE_BOOKMARK_PENDING";
+export const CREATE_BOOKMARK_SUCCESS =
+    "actions/bookmark/CREATE_BOOKMARK_SUCCESS";
+export const CREATE_BOOKMARK_FAIL = "actions/bookmark/CREATE_BOOKMARK_FAIL";
+
+/* thunks */
+export const createBookmark = (bundleId, bookmarkObj) => {
+    return (dispatch) => {
+        dispatch({ type: CREATE_BOOKMARK_PENDING });
+        bookmarkApi
+            .createBookmark(bundleId, bookmarkObj)
+            .then((res) => {
+                dispatch({ type: CREATE_BOOKMARK_SUCCESS });
+                // refresh selectedCollection
+                dispatch(selectedCollectionDuck.getCollection(res.data._id));
+            })
+            .catch((err) => {
+                dispatch({ type: CREATE_BOOKMARK_FAIL, payload: err });
+            });
+    };
+};
--- frontend/src/redux/api/bookmark.js
+++ frontend/src/redux/api/bookmark.js
@@ -5,6 +5,13 @@ import getAxiosConfig from "./getAxiosConfig";
 const url = process.env.REACT_APP_API_URL;
 
 /* create a bookmark in a bundle */
+export const createBookmark = (bundleId, bookmarkObj) => {
+    Axios.post(
+        url + "/bundles/" + bundleId + "/bookmarks",
+        { ...bookmarkObj },
+        getAxiosConfig()
+    );
+};
 
 /* modify a bookmark */
 
