068183348aba5e946f2aacdd841449f7967c910a
Jason Dip <ContactJasonDip@gmail.com>
2020-06-06 23:04:40 -0700

    add skeleton for bookmark api in frontend

---
+++ frontend/src/redux/api/bookmark.js
@@ -0,0 +1,13 @@
+import Axios from "axios";
+
+import getAxiosConfig from "./getAxiosConfig";
+
+const url = process.env.REACT_APP_API_URL;
+
+/* create a bookmark in a bundle */
+
+/* modify a bookmark */
+
+/* move a bookmark between bundles */
+
+/* delete a bookmark */
