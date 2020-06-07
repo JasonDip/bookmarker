f69e66cd362e22e40699b6df56a6f68883def695
Jason Dip <ContactJasonDip@gmail.com>
2020-06-07 00:33:00 -0700

    fix wording in AddFolderButton

--- frontend/src/components/SelectedCollection/Bundle/Buttons/AddFolderButton/AddFolderButton.js
+++ frontend/src/components/SelectedCollection/Bundle/Buttons/AddFolderButton/AddFolderButton.js
@@ -1,9 +1,8 @@
 import React from "react";
 import IconButton from "@material-ui/core/IconButton";
-import AddIcon from "@material-ui/icons/Add";
 import CreateNewFolderOutlinedIcon from "@material-ui/icons/CreateNewFolderOutlined";
 
-const AddButton = (props) => {
+const AddFolderButton = (props) => {
     const { clickHandler } = props;
     return (
         <IconButton style={props.style} aria-label="add" onClick={clickHandler}>
@@ -12,4 +11,4 @@ const AddButton = (props) => {
     );
 };
 
-export default AddButton;
+export default AddFolderButton;
