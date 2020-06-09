f8e3af03ddfc3d23aaa37c79325e7cd347da1ca6
Jason Dip <ContactJasonDip@gmail.com>
2020-06-09 02:23:19 -0700

    add about page

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,55 +1,86 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: "center",
+        color: theme.palette.text.secondary,
+    },
+}));
 
 const About = (props) => {
+    const classes = useStyles();
+
     return (
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            congue, ipsum at malesuada bibendum, nibh eros pretium odio, non
-            eleifend est arcu ac orci. Phasellus mauris justo, porttitor id
-            tempus at, ullamcorper nec odio. Aliquam leo tortor, elementum ac
-            dapibus eu, cursus sit amet sapien. Integer venenatis dolor et
-            ultricies aliquet. Duis consequat tellus vitae euismod placerat.
-            Nunc sagittis aliquam tempus. Praesent consequat suscipit neque, sit
-            amet rhoncus ex. Nulla finibus tristique porttitor. Aliquam sed
-            scelerisque erat. Mauris sodales congue lacus eu viverra. Morbi
-            suscipit massa a erat aliquam suscipit. Morbi ac gravida nisl, eu
-            dapibus ipsum. Aenean viverra turpis consectetur erat posuere
-            scelerisque. Mauris porta ex mauris, ut hendrerit ante mollis
-            placerat. Sed lacinia auctor erat vel sodales. Nullam et leo luctus,
-            luctus est id, pharetra elit. Suspendisse sed lectus faucibus,
-            placerat metus ac, iaculis ipsum. Suspendisse ac fringilla nisi.
-            Donec vitae velit dolor. Phasellus consectetur ut nunc eu commodo.
-            Nam interdum non enim sit amet aliquam. Sed placerat facilisis elit
-            sed rutrum. Etiam fermentum vestibulum eros eu ornare. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Vivamus at nunc quis
-            orci mattis pulvinar a eu ante. Vestibulum maximus pharetra gravida.
-            Aliquam lacus risus, varius quis justo et, maximus cursus velit.
-            Curabitur sagittis, ligula sit amet ornare fermentum, nisl turpis
-            interdum sem, nec pharetra ex massa in eros. Quisque quis lectus
-            venenatis, sodales leo sed, ultrices tortor. Maecenas ultrices nunc
-            mattis, ultrices orci ut, rutrum urna. Donec nec eros non purus
-            iaculis rhoncus. Morbi vestibulum arcu et dolor feugiat finibus quis
-            feugiat arcu. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Suspendisse luctus, risus et
-            condimentum iaculis, turpis massa facilisis eros, nec commodo odio
-            nisi ut sem. Maecenas rhoncus id leo vitae euismod. In hac habitasse
-            platea dictumst. Morbi luctus consequat aliquam. Cras sollicitudin,
-            libero vel placerat pulvinar, justo mauris pellentesque lectus, sed
-            ultricies lectus nulla volutpat eros. Cras efficitur, dolor congue
-            venenatis blandit, nibh velit congue leo, at varius velit neque eget
-            lorem. Cras ullamcorper facilisis purus, in sagittis quam maximus
-            at. Aliquam erat volutpat. Donec sollicitudin diam imperdiet,
-            euismod sem non, tempus nisi. Nulla sit amet euismod eros, ut
-            laoreet sem. Donec pulvinar interdum ex, sit amet euismod neque
-            facilisis sed. Sed ac auctor erat, et luctus purus. Maecenas arcu
-            mauris, facilisis in accumsan sit amet, maximus maximus orci. Ut
-            fringilla libero non mi euismod, et cursus urna placerat. Aliquam
-            elit nisl, lacinia quis auctor eget, tempus id lacus. Nunc vitae
-            viverra mauris. Donec eget elit at lacus molestie interdum. Proin
-            posuere elit nisl, quis egestas nisl ornare gravida. Ut mollis elit
-            sed risus feugiat placerat. Nam fringilla nunc eget nisl lacinia
-            facilisis.
-        </p>
+        <div className={classes.root}>
+            <Grid container>
+                <Grid item xs={0} md={3} lg={4} />
+                <Grid item xs={12} md={6} lg={4}>
+                    <Typography>
+                        <h1>About</h1>
+                        <p style={{ whiteSpace: "pre-line" }}>
+                            {`Bookmarker is a web app for storing and sharing bookmarks (hyperlinks) on the cloud. These collection of bookmarks can be organized into expandable folders and may have notes attached to them. Collections can be set to private (only the creator may view) or public (anyone may view).`}
+                        </p>
+                        <h1>How To Use</h1>
+                        <ol>
+                            <li>
+                                Create an account at the Sign Up page and then
+                                Log In.
+                            </li>
+                            <li>
+                                Using the left panel, create a collection or
+                                select an existing one.
+                            </li>
+                            <li>
+                                Activate Edit Mode to view the buttons used for
+                                modifying your collection.
+                            </li>
+                            <li>
+                                Add folders and bookmarks. Folders can be
+                                nested.
+                            </li>
+                            <li>
+                                Use the Share button to edit your collection's
+                                privacy settings.
+                            </li>
+                        </ol>
+                        <h1>Security</h1>
+                        <p style={{ whiteSpace: "pre-line" }}>
+                            {`Your email will never be shared. 
+                            Your password is hashed. 
+                            Data transfer is encrypted.`}
+                        </p>
+                        <h1>Contact</h1>
+                        <p style={{ whiteSpace: "pre-line" }}>
+                            <span>Me: </span>
+                            <a
+                                href="https://www.jasondip.com/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                JasonDip.com
+                            </a>
+                            <br />
+                            <span>GitHub: </span>
+                            <a
+                                href="https://github.com/JasonDip"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                github.com/JasonDip
+                            </a>
+                        </p>
+                    </Typography>
+                </Grid>
+                <Grid item xs={0} md={3} lg={4} />
+            </Grid>
+        </div>
     );
 };
 
