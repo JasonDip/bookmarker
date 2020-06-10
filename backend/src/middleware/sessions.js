ae749d0ea96c2f3e8c8f9d1d98a27fe4179f9bd5
Jason Dip <ContactJasonDip@gmail.com>
2020-06-09 18:43:17 -0700

    set secure cookies

--- backend/src/middleware/sessions.js
+++ backend/src/middleware/sessions.js
@@ -3,21 +3,29 @@ const MongoDBStore = require("connect-mongodb-session")(session);
 
 const store = new MongoDBStore({
     uri: process.env.MONGODB_URI,
-    collection: "sessions"
+    collection: "sessions",
 });
 
-store.on("error", function(error) {
+store.on("error", function (error) {
     throw new Error("[ERROR] Error initializing session store: " + error);
 });
 
-// TODO: add "secure: true" cookie setting after setting up https
+const cookieOBj =
+    process.env.NODE_ENV === "production"
+        ? {
+              httpOnly: true,
+              maxAge: 2.592e9,
+              secure: "true",
+          }
+        : {
+              httpOnly: true,
+              maxAge: 2.592e9,
+          };
+
 module.exports = session({
     secret: process.env.SESSION_SECRET,
     store: store,
     resave: false,
     saveUninitialized: false,
-    cookie: {
-        httpOnly: true,
-        maxAge: 2.592e9
-    }
+    cookie: cookieOBj,
 });
