87d5e51f3cd63aabaaced4ad535eb4448dbe34f9
Jason Dip <ContactJasonDip@gmail.com>
2021-06-23 18:38:01 -0600

    add multi domains in Access-Control-Allow-Origin

--- backend/src/middleware/CORS.js
+++ backend/src/middleware/CORS.js
@@ -3,9 +3,15 @@ module.exports = (req, res, next) => {
     // need to set allow-origin with specific domain and allow-credentials
     // or else cross domain cookies will not work
     //res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_DOMAIN);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
 
+    // https://stackoverflow.com/questions/24897801/enable-access-control-allow-origin-for-multiple-domains-in-node-js
+    const allowedOrigins = ['https://www.bookmarker.jasondip.com', 'https://bookmarker.jasondip.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader(
         "Access-Control-Allow-Methods",
         "OPTIONS, GET, POST, PATCH, DELETE"
